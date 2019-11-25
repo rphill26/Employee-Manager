9c9e9401c596460ab4938578239b549e814d4ae3
Randall Phillips <rphill45@uncc.edu>
2019-11-24 19:03:12 -0500

    added intial inquirer prompts,along with departments and roles

--- employee_manager_server.js
+++ employee_manager_server.js
@@ -11,5 +11,52 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+    start();
 });
+
+// Starting up the Employee Manager
+
+function start(){
+    // Asks user what they want to do
+    inquirer
+    .prompt({
+       name: "functionChoice",
+       type: "list",
+       message: "What would you like to do?",
+       choices: ["View all employees", "View all employees by Department", "View all employees by Manager"] 
+    })
+    .then((answer) => {
+        if (answer.functionChoice === "View all employees"){
+            readAllEmployees();
+        }
+        else if (answer.functionChoice === "View all employees by Department"){
+            readDepartments();
+        }
+        else if (answer.functionChoice === "View all employees by Manager"){
+            readManagers();
+        } else {
+            connection.end();
+        }
+    });
+}
+
+// Function to list all employees in the database
+
+function readAllEmployees() {
+    connection.query("SELECT * FROM employees", (err, results) =>{
+        if (err) throw err;
+        inquirer
+        .prompt({
+            name: "choices",
+            type: "rawlist",
+            choices: () => {
+                var employeeArray = [];
+                for (var i =0; i < results.length; i++){
+                    employeeArray.push(results[i].first_name + " " + results[i].last_name);
+                }
+                return employeeArray;
+            },
+            message: "Which employee would you like to view?"
+        })
+    });
+}
